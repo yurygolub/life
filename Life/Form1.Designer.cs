e3b1df357d660dcb37679e60caa87d7201511868
Yury Golub <yuragolub21@gmail.com>
2022-10-04 17:17:17 +0300

    Decrease the timer interval.

--- Life/Form1.Designer.cs
+++ Life/Form1.Designer.cs
@@ -181,7 +181,7 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 40;
+            this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
             // Form1
